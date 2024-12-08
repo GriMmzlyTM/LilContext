ba919c04c2bd2852f09916b2e22505e7673d9650
GrimmzlyTM <lorenzo@tortonmind.com>
2024-12-08 15:21:11 -0500

    feat: update url

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://grimmzlytm.github.io',
+  url: 'www.LilContext.lorenzotorelli.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/LilContext/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
